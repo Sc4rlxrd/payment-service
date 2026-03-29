887ab2c4a17ffd484c44326e1ca1a5e5f3c1eea3
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-03-29 10:22:09 -0300

    feat: add custom exceptions and fix database credential errors.

---
+++ src/main/java/com/scarlxrd/payment_service/exception/PaymentException.java
@@ -0,0 +1,7 @@
+package com.scarlxrd.payment_service.exception;
+
+public class PaymentException extends BusinessException {
+    public PaymentException(String message) {
+        super(message);
+    }
+}
