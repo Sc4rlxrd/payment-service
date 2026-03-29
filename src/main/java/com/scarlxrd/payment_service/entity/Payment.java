ee4fe45e746bbead87415e0f63a795facd6d730b
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-03-29 09:58:44 -0300

    feat: add entity and repository.

---
+++ src/main/java/com/scarlxrd/payment_service/entity/Payment.java
@@ -0,0 +1,38 @@
+package com.scarlxrd.payment_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID orderId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+
+    }
+}
