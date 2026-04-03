5d7acd355b7e0a3cf87c02ae6d1756b4ca08d99a
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-04-03 20:21:26 -0300

    feat(config): add Spring AMQP dependency and RabbitMQ infrastructure

---
+++ src/main/java/com/scarlxrd/payment_service/config/rabbitmq/RabbitConfig.java
@@ -0,0 +1,30 @@
+package com.scarlxrd.payment_service.config.rabbitmq;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+}
+
