e0a4f40c333b973684640873177938a92e3c4ddb
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-04-03 20:27:02 -0300

    feat(messaging): implement payment consumer and publisher

---
+++ src/main/java/com/scarlxrd/payment_service/config/rabbitmq/PaymentConsumer.java
@@ -0,0 +1,37 @@
+package com.scarlxrd.payment_service.config.rabbitmq;
+
+import com.scarlxrd.payment_service.dto.OrderCreatedEvent;
+import com.scarlxrd.payment_service.dto.PaymentResultEvent;
+import com.scarlxrd.payment_service.exception.PaymentException;
+import com.scarlxrd.payment_service.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentConsumer {
+
+    private final PaymentService paymentService;
+    private final PaymentPublisher paymentPublisher;
+
+    @RabbitListener(queues = "order.created.queue")
+    public void handleOrderCreated(OrderCreatedEvent event) {
+
+        log.info("Message received from order-service");
+        try {
+            PaymentResultEvent result = paymentService.process(event);
+
+            if ("SUCCESS".equals(result.getStatus())) {
+                paymentPublisher.publishSuccess(result);
+            } else {
+                paymentPublisher.publishFailed(result);
+            }
+
+        } catch (PaymentException ex) {
+            throw ex;
+        }
+    }
+}
---
+++ src/main/java/com/scarlxrd/payment_service/config/rabbitmq/PaymentPublisher.java
@@ -0,0 +1,31 @@
+package com.scarlxrd.payment_service.config.rabbitmq;
+
+import com.scarlxrd.payment_service.dto.PaymentResultEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publishSuccess(PaymentResultEvent event) {
+        rabbitTemplate.convertAndSend(
+                "book.events",
+                "payment.success",
+                event
+        );
+    }
+
+    public void publishFailed(PaymentResultEvent event) {
+        rabbitTemplate.convertAndSend(
+                "book.events",
+                "payment.failed",
+                event
+        );
+    }
+}
